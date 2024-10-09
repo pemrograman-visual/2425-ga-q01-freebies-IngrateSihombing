ce0383502eaad4db63dafad8b6e1d12f62bc2819
IngrateSihombing <ingratejoy@gmail.com>
2024-10-09 16:23:27 +0700

    Pair21

--- X01.java
+++ X01.java
@@ -1,2 +1,35 @@
-// NIM - Your Name
-// NIM - Your Name
+// 12S24015-LucasPardede
+// 12S24022-IngrateJoySihombing
+import java.util.*;
+import java.lang.Math;
+
+public class X01 {
+    private static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        double bookprice, totalprice, lowestprice, totaltransaction;
+
+        bookprice = Double.parseDouble(input.nextLine());
+        totalprice = bookprice;
+        lowestprice = bookprice;
+        while (bookprice != 0) {
+            if (lowestprice < bookprice) {
+                lowestprice = lowestprice;
+            } else {
+                lowestprice = bookprice;
+            }
+            bookprice = Double.parseDouble(input.nextLine());
+            totalprice = totalprice + bookprice;
+        }
+        if (totalprice >= 100) {
+            totaltransaction = totalprice - lowestprice;
+        } else {
+            totaltransaction = totalprice;
+        }
+        System.out.println(toFixed(totaltransaction,2));
+    }
+    
+    private static String toFixed(double value, int digits) {
+        return String.format("%." + digits + "f", value);
+    }
+}
